2e7cdf39677ca46e0387b7fdaa0f20ae0e9a63a5
aqinglee112233 <110798972+aqinglee112233@users.noreply.github.com>
2023-05-02 14:37:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,37 +4,24 @@ import os
 import openai
 app = Flask(__name__)
 openai.api_key = os.environ.get("YOUR_API_KEY")
-@app.route('/gpt', methods=['POST'])
-def call_openai_api():
-    prompt = request.form.get('prompt')
-    model = request.form.get('model', 'text-davinci-002')
-    max_tokens = int(request.form.get('maxTokens', 16))
-    temperature = float(request.form.get('temperature', 0.7))
-    print(prompt)
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': "Api-key  " + openai.api_key
-    }
-    data = {
-        "prompt": prompt,
-        "model": model,
-        "max_tokens": max_tokens,
-        "temperature": temperature
-    }
-    print(headers,data )
-    response = requests.post('https://api.openai.com/v1/engines/davinci-codex/completions', headers=headers, json=data)
+@app.route('/GPT', methods=['POST'])
+def complete_text():
+    prompt = request.json.get('prompt')
+    model = request.json.get('model', 'text-davinci-002')
+    max_tokens = int(request.json.get('max_tokens', 16))
+    temperature = float(request.json.get('temperature', 0.7))
+
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature
+    )
 
     if response.status_code == 200:
-        result = response.json()
-        return jsonify({
-            'success': True,
-            'response': result['choices'][0]['text']
-        })
+        return jsonify({'response': response.choices[0].text})
     else:
-        return jsonify({
-            'success': False,
-            'error': f'{response.status_code}: {response.reason}'
-        })
+        return jsonify({'error': f'{response.status_code}: {response.reason}'})
 
 if __name__ == '__main__':
       port = int(os.environ.get("PORT"))
