2df31f62f2c62b33f131dcecd963e7d3208c42e5
aqinglee112233 <110798972+aqinglee112233@users.noreply.github.com>
2023-05-02 15:30:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,26 @@ import os
 import openai
 app = Flask(__name__)
 openai.api_key = os.environ.get("YOUR_API_KEY")
-@app.route('/GPT', methods=['POST'])
-def complete_text():
+@app.route('/gpt', methods=['POST'])
+def gpt():
     prompt = request.json.get('prompt')
     model = request.json.get('model', 'text-davinci-002')
     max_tokens = int(request.json.get('max_tokens', 16))
     temperature = float(request.json.get('temperature', 0.7))
-
+    data = {
+        'prompt': prompt,
+        'engine': model ,
+        'max_tokens': max_tokens,
+        'temperature': temperature
+          }
     response = openai.Completion.create(
         engine=model,
         prompt=prompt,
         max_tokens=max_tokens,
-        temperature=temperature
+        temperature = temperature
     )
-
-    if response.status_code == 200:
-        return jsonify({'response': response.choices[0].text})
-    else:
-        return jsonify({'error': f'{response.status_code}: {response.reason}'})
+    print(response.status_code)
+    return response
 
 if __name__ == '__main__':
       port = int(os.environ.get("PORT"))
